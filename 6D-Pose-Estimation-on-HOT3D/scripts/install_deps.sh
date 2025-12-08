e326e5ed6d527da7c54b4a5a51366aecf022e17e
jikenyg <167830683+jikenyg@users.noreply.github.com>
2025-12-08 16:31:25 +0800

    Delete 6D-Pose-Estimation-on-HOT3D/scripts directory

--- 6D-Pose-Estimation-on-HOT3D/scripts/install_deps.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-conda create -n 6dHOT3D -y python=3.10.13
-conda activate 6dHOT3D
-conda install -y pytorch==1.13.1 torchvision==0.14.1 torchaudio==0.13.1 pytorch-cuda=11.7 -c pytorch -c nvidia
-pip install "git+https://github.com/facebookresearch/pytorch3d.git@stable"
-pip install -r requirements.txt
-pip install "git+https://github.com/thodan/bop_toolkit"
-pip install spatial-correlation-sampler==0.4.0
